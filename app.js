535b54692c24241927c68b835ae16858df17e7fd
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-18 17:17:56 -0400

    Instantiate the Captain and alenShip class in Game class

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ class Captain {
 
 class Game {
     constructor(CaptainName) {
-       
+        this.captain = new Captain(CaptainName, 100);
+        this.alienShips = [
+            new AlienShip("X", 20),
+            new AlienShip("Z", 25),
+            new AlienShip("T", 30),
+            new AlienShip("X", 20),
+            new AlienShip("V", 25),
+            new AlienShip("K", 30),
+        ];
+        this.currentAlienShip = this.alienShips[0];
     }
 }
 
